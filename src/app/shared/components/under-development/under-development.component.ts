6e47c9621c2d9d8707d65adff581c0584835c4d0
Jeewan-Sharma <sjeewan53@gmail.com>
2023-07-04 01:34:20 +0545

    cart clear after sucess Fix

--- src/app/shared/components/under-development/under-development.component.ts
+++ src/app/shared/components/under-development/under-development.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-under-development',
   templateUrl: './under-development.component.html',
   styleUrls: ['./under-development.component.scss'],
 })
-export class UnderDevelopmentComponent {
-  constructor(private router: Router) {}
+export class UnderDevelopmentComponent implements OnInit {
+  constructor(private router: Router, private cartService: CartService) {}
+  ngOnInit(): void {
+    this.cartService.clearCart();
+  }
   returnToHome() {
     this.router.navigate(['/']);
   }
