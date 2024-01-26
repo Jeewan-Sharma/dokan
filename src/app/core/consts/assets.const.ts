66377a62d6ed204d75a1c15ba88d7edde77cbab8
Jeewan Sharma <sjeewan53@gmail.com>
2024-01-26 21:59:24 +1100

    feat: category section complete

--- src/app/core/consts/assets.const.ts
+++ src/app/core/consts/assets.const.ts
@@ -2,7 +2,12 @@ const IMAGE_BASE = 'assets/images';
 
 const IMAGES = {
   LOGO: `${IMAGE_BASE}/logo.png`,
-  BANNER_LADY: `${IMAGE_BASE}/bannerLady.png`,
+  BANNER_LADY: `${IMAGE_BASE}/modals/bannerLady.png`,
+  WOMEN: `${IMAGE_BASE}/modals/women.png`,
+  BAGS: `${IMAGE_BASE}/modals/bags.png`,
+  SHOES: `${IMAGE_BASE}/modals/shoes.png`,
+  WATCHES: `${IMAGE_BASE}/modals/watches.png`,
+  MEN: `${IMAGE_BASE}/modals/men.png`,
 }
 
 
